cc60ba8f8640192b9bb001160d11145ee33c4028
piggy.park <www.piggy.park@groundx.xyz>
2023-07-13 17:24:39 +0900

    cluster identifier 업데이트 추가

--- StepCountApp/MapViewUIKit/MapAnnotations/PointSpotAnnotation.swift
+++ StepCountApp/MapViewUIKit/MapAnnotations/PointSpotAnnotation.swift
@@ -20,6 +20,12 @@ final class PointSpotAnnotationView: MKAnnotationView {
 //        setupGestureRecognizers()
     }
 
+    override var annotation: MKAnnotation? {
+        willSet {
+            clusteringIdentifier = pointSpotClusterID
+        }
+    }
+
     @available(*, unavailable)
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
