6d00ff401f6f5f2c0907dee942880b04bd6c5118
Piggy(박진섭) <piggy.park@groundx.xyz>
2023-12-27 17:41:30 +0900

    add sample test code

---
+++ StepCountAppTests/StepCountAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StepCountAppTests.swift
+//  StepCountAppTests
+//
+//  Created by gx_piggy on 12/27/23.
+//
+
+import XCTest
+
+final class StepCountAppTests: XCTestCase {
+
+    func testExample() throws {
+        let a = 1
+        let b = 2
+        XCTAssertTrue(3 == (a + b))
+    }
+}
