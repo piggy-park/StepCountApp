b12e027151efbccba68f3af09634b1901c59e57f
Piggy(박진섭) <piggy.park@groundx.xyz>
2024-01-04 11:00:32 +0900

    요약된 정보로 나오게 script 수정

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,37 +7,40 @@
 #  
 echo "post clone script phase"
 
-# 운영 체제 정보 출력
-echo "==== Operating System ===="
-echo "OS: $(uname -s)"
-echo "Kernel Version: $(uname -r)"
+uname -m
+system_profiler SPSoftwareDataType SPHardwareDataType
 
-# 사용 가능한 쉘 정보 출력
-echo -e "\n==== Shell Information ===="
-echo "Shell: $SHELL"
-echo "Available Shells: $(cat /etc/shells)"
-
-# 현재 사용자 정보 출력
-echo -e "\n==== User Information ===="
-echo "Logged-in User: $USER"
-echo "Home Directory: $HOME"
-
-echo "==== CPU Information ===="
-echo "Processor: $(sysctl -n machdep.cpu.brand_string)"
-echo "Number of Cores: $(sysctl -n hw.ncpu)"
-
-# 메모리(RAM) 정보 출력
-echo -e "\n==== Memory Information ===="
-echo "Total Memory: $(sysctl -n hw.memsize | awk '{print $0/1024^3 " GB"}')"
-echo "Available Memory: $(vm_stat | grep "Pages free:" | awk '{print $3*4096/1024^3 " GB"}')"
-
-# 디스크 용량 정보 출력
-echo -e "\n==== Disk Information ===="
-df -h | grep '/dev/' | awk '{print "File System: " $1, "\nSize: " $2, "\nUsed: " $3, "\nAvailable: " $4, "\nUsage: " $5, "\nMount Point: " $6, "\n---"}'
-
-# 네트워크 인터페이스 정보 출력
-echo -e "\n==== Network Information ===="
-ifconfig | grep -E '(en0|en1|eth0|eth1|wlan0|wlan1)' -A 5
+## 운영 체제 정보 출력
+#echo "==== Operating System ===="
+#echo "OS: $(uname -s)"
+#echo "Kernel Version: $(uname -r)"
+#
+## 사용 가능한 쉘 정보 출력
+#echo -e "\n==== Shell Information ===="
+#echo "Shell: $SHELL"
+#echo "Available Shells: $(cat /etc/shells)"
+#
+## 현재 사용자 정보 출력
+#echo -e "\n==== User Information ===="
+#echo "Logged-in User: $USER"
+#echo "Home Directory: $HOME"
+#
+#echo "==== CPU Information ===="
+#echo "Processor: $(sysctl -n machdep.cpu.brand_string)"
+#echo "Number of Cores: $(sysctl -n hw.ncpu)"
+#
+## 메모리(RAM) 정보 출력
+#echo -e "\n==== Memory Information ===="
+#echo "Total Memory: $(sysctl -n hw.memsize | awk '{print $0/1024^3 " GB"}')"
+#echo "Available Memory: $(vm_stat | grep "Pages free:" | awk '{print $3*4096/1024^3 " GB"}')"
+#
+## 디스크 용량 정보 출력
+#echo -e "\n==== Disk Information ===="
+#df -h | grep '/dev/' | awk '{print "File System: " $1, "\nSize: " $2, "\nUsed: " $3, "\nAvailable: " $4, "\nUsage: " $5, "\nMount Point: " $6, "\n---"}'
+#
+## 네트워크 인터페이스 정보 출력
+#echo -e "\n==== Network Information ===="
+#ifconfig | grep -E '(en0|en1|eth0|eth1|wlan0|wlan1)' -A 5
 
 # 그래픽 카드 정보 출력 (Mac에서만 동작)
 #echo -e "\n==== GPU Information ===="
